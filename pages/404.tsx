dc4e3621ebf857b55a93e6b7a91d651a7c95555f
SolijonovDev <solijonovqodirjon@gmail.com>
2022-06-14 14:06:36 +0500

    init

---
+++ pages/404.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "../components/Layout";
+
+
+export default function NotFound(){
+    return (
+        <MainLayout title="Not found">
+            <h1>Not found</h1>
+        </MainLayout>
+    )
+}
