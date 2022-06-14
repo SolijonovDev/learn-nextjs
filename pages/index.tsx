babf7e254d59ca35f101f4f0d7e9fdccb4823437
SolijonovDev <solijonovqodirjon@gmail.com>
2022-06-14 16:38:19 +0500

    added eslint and stylelint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import { NextPage } from "next";
 
 
-const Home:NextPage=()=>{
+const Home=():JSX.Element=>{
     return (
         <div>
             <h1>Home page</h1>
         </div>
-    )
-}
+    );
+};
 
 export default Home;
